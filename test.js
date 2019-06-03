d003a501b09f3e8fa15ac31a66bd97185dd3e691
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-06-03 12:27:29 +0100

    upgrade lws

--- test.js
+++ test.js
@@ -7,7 +7,6 @@ const BasicAuth = require('./')
 const tom = module.exports = new Tom('basic-auth')
 
 tom.test('no config', async function () {
-  const lws = new Lws()
   const port = 9000 + this.index
   class One {
     middleware () {
@@ -17,19 +16,18 @@ tom.test('no config', async function () {
       }
     }
   }
-  const server = lws.listen({
+  const lws = Lws.create({
     port,
     stack: [ BasicAuth, One ]
   })
   const response = await fetch(`http://localhost:${port}/`)
-  server.close()
+  lws.server.close()
   a.strictEqual(response.status, 200)
   const body = await response.text()
   a.strictEqual(body, 'one')
 })
 
 tom.test('username and password: unauthorised', async function () {
-  const lws = new Lws()
   const port = 9000 + this.index
   class One {
     middleware () {
@@ -39,19 +37,18 @@ tom.test('username and password: unauthorised', async function () {
       }
     }
   }
-  const server = lws.listen({
+  const lws = Lws.create({
     port,
     stack: [ BasicAuth, One ],
     authUser: 'user',
     authPass: 'password'
   })
   const response = await fetch(`http://localhost:${port}/`)
-  server.close()
+  lws.server.close()
   a.strictEqual(response.status, 401)
 })
 
 tom.test('username and password: authorised', async function () {
-  const lws = new Lws()
   const port = 9000 + this.index
   class One {
     middleware () {
@@ -61,14 +58,14 @@ tom.test('username and password: authorised', async function () {
       }
     }
   }
-  const server = lws.listen({
+  const lws = Lws.create({
     port,
     stack: [ BasicAuth, One ],
     authUser: 'user',
     authPass: 'password'
   })
   const response = await fetch(`http://user:password@localhost:${port}/`)
-  server.close()
+  lws.server.close()
   a.strictEqual(response.status, 200)
   const body = await response.text()
   a.strictEqual(body, 'one')
